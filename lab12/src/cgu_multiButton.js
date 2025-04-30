1c4da7bdb03123d1b975d334486497ce51d4c79b
mnbvcxz1209 <151009588+mnbvcxz1209@users.noreply.github.com>
2025-04-30 14:43:21 +0800

    Add files via upload

---
+++ lab12/src/cgu_multiButton.js
@@ -0,0 +1,17 @@
+import Button from "@mui/material/Button";
+
+
+//const styleArgument = {fontSize: '100px',color:'red'};
+const changeText=(event)=>{
+    console.log(event.target)
+    event.target.innerText = event.target.innerText +"被點了"
+  }
+  const MultiButton=(num)=>{
+    var output=[];
+    for(let i=1;i<num+1;++i)
+      output.push(<Button variant="contained" color="primary"       
+      onClick={changeText}> 第{i}個按鍵</Button>)
+    return output;
+  }
+
+  export default MultiButton;
